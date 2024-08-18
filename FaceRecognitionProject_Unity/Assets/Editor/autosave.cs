fa0a4144d78c8b74236be67ad2172f1a05892d42
RoosterWho <gonghq@shanghaitech.edu.cn>
2024-08-18 18:45:58 +0800

    FaceSystem

---
+++ FaceRecognitionProject_Unity/Assets/Editor/autosave.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+ 
+[InitializeOnLoad]
+public class AutoSave
+{
+    static AutoSave()
+    {
+        EditorApplication.playModeStateChanged += (PlayModeStateChange state) => {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
+
+//put this under Assets/Editor/autosave.cs
--- FaceRecognitionProject_Unity/Assets/OpenCVForUnity/Examples/MainModules/dnn/FacialExpressionRecognitionExample/FaceExpressionReaderManager.cs
+++ FaceRecognitionProject_Unity/Assets/OpenCVForUnity/Examples/MainModules/dnn/FacialExpressionRecognitionExample/FaceExpressionReaderManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using OpenCVForUnityExample;
+using System.Linq;
 
 public class FaceExpressionReaderManager : MonosingletonTemp<FaceExpressionReaderManager>
 {
@@ -9,15 +10,49 @@ public class FaceExpressionReaderManager : MonosingletonTemp<FaceExpressionReade
     public string currentEmotionLabel;
     public float currentEmotionValue;
 
+    public List<string> emotionLabels = new List<string>();
+    public int listLength = 100; // Increase the list length
+    public float sampleInterval = 1f;
+    private float lastSampleTime = 0f;
+    public string SampledEmotionLabel { get; private set; }
+
     void Update()
     {
         currentEmotionLabel = facialExpressionRecognitionExample.GetCurrentEmotionLabel();
         currentEmotionValue = facialExpressionRecognitionExample.GetCurrentEmotionConfidence();
 
         // Do something with the emotion label and value
-        Debug.Log("Emotion: " + currentEmotionLabel + ", Confidence: " + currentEmotionValue);
+        //Debug.Log("Emotion: " + currentEmotionLabel + ", Confidence: " + currentEmotionValue);
+
+        // Sample emotion label at intervals
+        if (Time.time - lastSampleTime > sampleInterval)
+        {
+            lastSampleTime = Time.time;
+
+            // If current emotion label is empty, set it to "neutral"
+            if (string.IsNullOrEmpty(currentEmotionLabel))
+            {
+                currentEmotionLabel = "neutral";
+            }
+
+            // Add current emotion label to list
+            emotionLabels.Add(currentEmotionLabel);
+
+            // Limit the list to the last 'listLength' elements
+            if (emotionLabels.Count > listLength)
+            {
+                emotionLabels.RemoveAt(0);
+            }
+
+            // Update the most common emotion label
+            SampledEmotionLabel = emotionLabels.GroupBy(x => x)
+                                               .OrderByDescending(x => x.Count())
+                                               .First().Key;
+        }
+
+        Debug.Log(GetSampledEmotionLabel(2f));
     }
-    
+
     public string GetEmotionLabel()
     {
         return currentEmotionLabel;
@@ -26,5 +61,25 @@ public class FaceExpressionReaderManager : MonosingletonTemp<FaceExpressionReade
     public float GetEmotionValue()
     {
         return currentEmotionValue;
+    }
+
+    public string GetSampledEmotionLabel()
+    {
+        return SampledEmotionLabel;
+    }
+
+    // New function to get the most common emotion label in the last 'time' seconds
+    public string GetSampledEmotionLabel(float time)
+    {
+        int count = Mathf.RoundToInt(time / sampleInterval);
+        if (count > emotionLabels.Count)
+        {
+            count = emotionLabels.Count;
+        }
+
+        var recentEmotionLabels = emotionLabels.Skip(emotionLabels.Count - count);
+        return recentEmotionLabels.GroupBy(x => x)
+                                  .OrderByDescending(x => x.Count())
+                                  .First().Key;
     }
 }
